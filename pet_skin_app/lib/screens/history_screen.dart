e1716aa8c12247eed111433e2f4872de6252e276
Supuni-Punsarani <supunipunsarani1@gmail.com>
2026-07-07 20:39:24 +0530

    Fix AuthProvider naming collision in HistoryScreen

--- pet_skin_app/lib/screens/history_screen.dart
+++ pet_skin_app/lib/screens/history_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide AuthProvider;
 import 'package:provider/provider.dart';
 import '../providers/auth_provider.dart';
 import '../theme/app_theme.dart';
